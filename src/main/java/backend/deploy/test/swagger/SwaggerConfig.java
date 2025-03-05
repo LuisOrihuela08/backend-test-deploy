f387da1859af686b14e6ba1058b76cf361b91501
Tu Nombre <luis.orihuela@upeu.edu.pe>
2025-03-05 12:10:10 -0500

    configurando swagger para peticiones http y https

--- src/main/java/backend/deploy/test/swagger/SwaggerConfig.java
+++ src/main/java/backend/deploy/test/swagger/SwaggerConfig.java
@@ -1,18 +1,28 @@
 package backend.deploy.test.swagger;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class SwaggerConfig {
 
 	@Bean
-	public OpenAPI custonOpenAPI() {
+	public OpenAPI custonOpenAPI(HttpServletRequest request) {
+		
+		String scheme = request.getScheme();//Aca obtengo dinamicamente si es http o https
+		String serverUrl = scheme + "://" + request.getServerName();
+		
 		return new OpenAPI().info(new Info().title("API de prueba para despliegue")
 											.version("1.0.0")
-											.description("Esta documentacion de API  de prueba para intentar desplegarlo a distintas plataformas - creado por Luiz Beto"));
+											.description("Esta documentacion de API  de prueba para intentar desplegarlo a distintas plataformas - creado por Luiz Beto"))
+							.servers(List.of(new Server().url(serverUrl).description("Dynamic Server")));
+											
 	}
 }
