3b9397afa480b87cecca24e20e70ee58b8bb7df8
Tu Nombre <luis.orihuela@upeu.edu.pe>
2025-03-05 12:33:31 -0500

    arreglando la configuracion de swagger

--- src/main/java/backend/deploy/test/swagger/SwaggerConfig.java
+++ src/main/java/backend/deploy/test/swagger/SwaggerConfig.java
@@ -8,21 +8,24 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
-import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class SwaggerConfig {
 
-	@Bean
-	public OpenAPI custonOpenAPI(HttpServletRequest request) {
-		
-		String scheme = request.getScheme();//Aca obtengo dinamicamente si es http o https
-		String serverUrl = scheme + "://" + request.getServerName();
-		
-		return new OpenAPI().info(new Info().title("API de prueba para despliegue")
-											.version("1.0.0")
-											.description("Esta documentacion de API  de prueba para intentar desplegarlo a distintas plataformas - creado por Luiz Beto"))
-							.servers(List.of(new Server().url(serverUrl).description("Dynamic Server")));
-											
-	}
+	private final ServerUrlProvider serverUrlProvider;
+
+    public SwaggerConfig(ServerUrlProvider serverUrlProvider) {
+        this.serverUrlProvider = serverUrlProvider;
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de prueba para despliegue")
+                        .version("1.0.0")
+                        .description("Esta documentación de API de prueba para intentar desplegarlo en distintas plataformas - creado por Luiz Beto"))
+                .servers(List.of(new Server().url(serverUrlProvider.getServerUrl()).description("Dynamic Server")));
+    }
+
 }
